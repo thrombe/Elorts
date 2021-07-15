e8d4d711d7f90f2d8112d94ea85b90e72462e072
thrombe <yash.n.thombare620@gmail.com>
2021-07-15 14:59:33 +0000

    split code in multiple files

--- src/remind.rs
+++ src/remind.rs
@@ -1,85 +1,20 @@
 #![allow(unused_imports)]
 
-use reqwest;
-use std::collections::HashMap;
 use serde_derive::{Serialize, Deserialize};
-use serde_json::{json, to_string, from_str};
 
-struct Dweet<'a> {
-    key: &'a str,
-    get_link: String,
-    post_link: String,
-}
-
-impl<'a> Dweet<'a> {
-    fn new(dweet: &'a str) -> Self {
-        Self {
-            key: dweet,
-            get_link: format!("https://dweet.io/get/latest/dweet/for/{}", dweet),
-            post_link: format!("https://dweet.io/dweet/for/{}", dweet),
-        }
-    }
-    
-    /// get the data stored in dweep and deserialise it into a vec of Reminders
-    fn get_data(&self) -> Vec<Reminder> {
-        let resp = reqwest::blocking::get(&self.get_link).unwrap()
-            .text().unwrap();
-        let resp0: serde_json::Value = from_str(&resp).unwrap();
-        let resp1 = match resp0["with"][0]["content"].clone() {
-            serde_json::Value::Object(val) => val,
-            _ => panic!(),
-        };
-        // let resp2: HashMap<u64, Reminder<'_>> = from_str(&resp1).unwrap();
-        
-        let mut reminder_vec = Vec::new();
-        for (_, rem) in resp1.iter() {
-            reminder_vec.append(&mut vec![ // stuffing reminders in a vec to return
-                Reminder { // manually deserialising reminders from the serde_json object
-                    title: match rem["title"].clone() { // i had to do clones here to save myself from madness
-                        serde_json::Value::String(val) => val,
-                        _ => panic!(),
-                    },
-                    message: match rem["message"].clone() {
-                        serde_json::Value::String(val) => val,
-                        _ => panic!(),
-                    },
-                    time: match rem["time"].clone() {
-                        serde_json::Value::String(val) => val,
-                        _ => panic!(),
-                    },
-                }
-            ]);
-        }
-        reminder_vec
-    }
-    
-    /// dosent do anything useful for now, but can be used to post hashmaps of Reminders in dweet
-    fn post_data(&self) {
-        let mut data = HashMap::<u64, Reminder>::new(); // creating a test value for dweet
-        data.insert(0, Reminder {
-            title: "testle".to_string(),
-            message: "sawkon these".to_string(),
-            time: "bowls".to_string(),
-        });
-        
-        let client = reqwest::blocking::Client::new();
-        let res = client.post(&self.post_link)
-            .json(&data)
-            .send();
-    }
-}
+use super::dweet::Dweet;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Reminder {
-    title: String,
-    message: String,
-    time: String,
+pub struct Reminder {
+    pub title: String,
+    pub message: String,
+    pub time: String,
 }
 
 /// this is the main func here
 pub fn remind() -> Result<(), Box<dyn std::error::Error>> {
     let dweet = Dweet::new("beso-beso-beminders");
-    dweet.post_data();
+    // dweet.post_data();
     let data = dweet.get_data();
     println!("{:?}", data);
 
