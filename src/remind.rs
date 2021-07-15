86a0e8c3f1315b87a1e68a4f48f8e62cedc26b9d
thrombe <yash.n.thombare620@gmail.com>
2021-07-15 12:50:53 +0000

    starting to do this properly after this

--- src/remind.rs
+++ src/remind.rs
@@ -11,38 +11,7 @@ pub fn remind() -> Result<(), Box<dyn std::error::Error>> {
     let dweet_post = format!("https://dweet.io/dweet/for/{}", dweet);
     
     
-    /*
-    let data = discord {
-        content: "stuffshhsh",
-    };
-    
-    let webhook = "https://discord.com/api/webhooks/852818463734890511/CHWDqR7OLTJtDyudDVsFnkq7vHRDgbIx2fe7PIA_h-RiYErQLpnkgzmgyuS0HQe26urp";
-    let client = reqwest::blocking::Client::new();
-    let res = client.post(webhook)
-        .json(&data)
-        .send();
-    println!("{:?}", res);
-    */
-    
-    
-    let bec = Reminder { // serialise anything in hashmaps/structs (vecs dont work idk why)
-        title: "testRem",
-        message: "reminder to koff",
-        time: "68419",
-    };
-    let client = reqwest::blocking::Client::new();
-    let res = client.post(dweet_post)
-        .json(&bec)
-        .send();
-    println!("{:?}", res);
-    /*
-    let resp = reqwest::blocking::get(dweet_get).unwrap()
-        .text().unwrap();
-        //["with"][0]["content"];
-    println!("{:?}", &resp);
-    // let resp: Vec<Reminder> = from_str(&resp).unwrap();
-    // println!("{:#?}", resp);
-    */
+
     Ok(())
 }
 
@@ -54,9 +23,26 @@ struct Reminder<'a> {
     time: &'a str,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct discord<'a> {
-    content: &'a str,
+
+fn discord_stuff() {
+    
+    #[derive(Serialize, Deserialize, Debug)]
+    struct discord<'a> {
+        content: &'a str,
+    }
+    
+    
+    let data = discord {
+        content: "stuffshhsh",
+    };
+    
+    let webhook = "https://discord.com/api/webhooks/852818463734890511/CHWDqR7OLTJtDyudDVsFnkq7vHRDgbIx2fe7PIA_h-RiYErQLpnkgzmgyuS0HQe26urp";
+    let client = reqwest::blocking::Client::new();
+    let res = client.post(webhook)
+        .json(&data)
+        .send();
+    println!("{:?}", res);
+    
 }
 
 fn eg_ser_deser() -> Result<(), Box<dyn std::error::Error>> {
